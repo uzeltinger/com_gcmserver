8e4533923a302c3de51c6270323cc3efdf6496a3
uzeltinger <fabiouz@gmail.com>
2016-03-30 19:28:36 -0300

    Added files via upload

---
+++ admin/helpers/helper.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @version		$Id: properties.php 1 2010-2014 este8an $
+ * @package		Joomla.Administrator
+ * @subpackage	com_properties
+ * @copyright	Copyright (C) 2008 - 2016 Fabio Esteban Uzeltinger.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die;
+class GcmserverHelper //extends JHelperContent
+{
+	public static $extention = 'com_gcmserver';
+	
+	public static function addSubmenu($vName)
+	{		
+	$user = JFactory::getUser();
+	$userId	= $user->get('id');	
+	//$manageProduct = $user->authorise('core.manage.product', 'com_gcmserver');	
+	$coreAdmin = $user->authorise('core.admin', 'com_gcmserver');
+
+	if($coreAdmin)
+		{
+		JHtmlSidebar::addEntry(
+			JText::_('COM_GCMSERVER_MENU_PANEL'),
+			'index.php?option=com_gcmserver&view=panel',
+			$vName == 'panel'
+		);		
+		JHtmlSidebar::addEntry(
+			JText::_('COM_GCMSERVER_MENU_PROFILES'),
+			'index.php?option=com_gcmserver&view=profiles',
+			$vName == 'profiles'
+		);
+		JHtmlSidebar::addEntry(
+			JText::_('COM_GCMSERVER_MENU_NOTIFICATIONS'),
+			'index.php?option=com_gcmserver&view=notifications',
+			$vName == 'notifications'
+		);	
+		}
+				
+	}
+	
+	public static function getActions()
+	{
+		$user	= JFactory::getUser();
+		$result	= new JObject;
+		$actions = array(
+			'core.admin', 'core.manage', 'core.create', 'core.delete', 'core.edit', 'core.edit.state'
+		);
+		foreach ($actions as $action) {
+			$result->set($action,	$user->authorise($action, 'com_gcmserver'));
+		}
+		return $result;
+	}	
+}
