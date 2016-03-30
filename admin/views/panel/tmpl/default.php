7c05431096dcf57b77bab9300bb190f0645f7e72
uzeltinger <fabiouz@gmail.com>
2016-03-30 19:21:44 -0300

    Added files via upload

---
+++ admin/views/panel/tmpl/default.php
@@ -0,0 +1,56 @@
+﻿<?php
+/**
+ * @version		$Id: properties.php 1 2006-2016 este8an $
+ * @package		Joomla.Administrator
+ * @subpackage	com_properties
+ * @copyright	Copyright (C) 2006 - 2016 Fabio Esteban Uzeltinger.
+ * @email		fabiouz@gmail.com
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// Disallow direct access to this file
+defined('_JEXEC') or die('Restricted access');
+$Version='5.20120708';
+?>
+<div class="row-fluid">
+
+
+<?php if (!empty( $this->sidebar)) : ?>
+	<div id="j-sidebar-container" class="span2">
+		<?php echo $this->sidebar; ?>
+	</div>
+	<div id="j-main-container" class="span10">
+<?php else : ?>
+	<div id="j-main-container">
+<?php endif;?>	
+
+
+
+
+
+
+	<div class="row-fluid">
+		<div class="well well-small">
+            <h2 class="module-title nav-header">More Visited</h2>            
+            <div class="row-striped">
+            
+            
+                 
+                    
+			</div>
+			</div>		
+		</div>
+        
+        
+
+
+    </div>
+
+
+
+
+		 
+        
+
+
+</div>	<!--	row-fluid	--> 
+
