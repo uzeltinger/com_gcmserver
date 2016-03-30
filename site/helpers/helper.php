d3669e78e963d3eafef50b446a345f9972ce67c6
uzeltinger <fabiouz@gmail.com>
2016-03-30 19:17:09 -0300

    Added files via upload

---
+++ site/helpers/helper.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @version		$Id: properties.php 1 2006-2016 este8an $
+ * @package		Joomla.Administrator
+ * @subpackage	com_properties
+ * @copyright	Copyright (C) 2008 - 2016 Fabio Esteban Uzeltinger.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// no direct access
+defined('_JEXEC') or die('Restricted access'); 
+class PropertiesHelper
+{	
+	
+}
