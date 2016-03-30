0a55e954ed0f3e2f1eb55794c00628d370a37a07
uzeltinger <fabiouz@gmail.com>
2016-03-30 19:19:02 -0300

    Added files via upload

---
+++ site/views/gcmserver/tmpl/default.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @version		$Id: gcmserver.php 1 2006-2016 este8an $
+ * @package		Joomla.Administrator
+ * @subpackage	com_gcmserver
+ * @copyright	Copyright (C) 2008 - 2016 Fabio Esteban Uzeltinger.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// no direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.filesystem.file');	
+$params		= JComponentHelper::getParams('com_gcmserver');
+//require_once( JPATH_COMPONENT.'/helpers/params.php' );
+?>
+
+gcmserver view
