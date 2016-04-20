0ee9d9d83688a77551bcdedd21901d7a1ec2c449
uzeltinger <fabiouz@gmail.com>
2016-04-20 17:35:38 -0300

    Update profiles.php

--- admin/tables/profiles.php
+++ admin/tables/profiles.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @package		Joomla.Administrator
+ * @subpackage	com_gcmserver
+ * @copyright	Copyright (C) 2006 - 2016 Fabio Esteban Uzeltinger.
+ * @email		fabiouz@gmail.com
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined( '_JEXEC' ) or die( 'Restricted access' );
 class GcmserverTableprofiles extends JTable
 {    
