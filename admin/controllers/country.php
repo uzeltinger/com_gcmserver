ffcd06c6a54b9e9ce7ad85cc5dc7880c370cd302
uzeltinger <fabiouz@gmail.com>
2016-04-20 17:34:49 -0300

    Update country.php

--- admin/controllers/country.php
+++ admin/controllers/country.php
@@ -1,5 +1,12 @@
 <?php
-// no direct access
+/**
+ * @package		Joomla.Administrator
+ * @subpackage	com_gcmserver
+ * @copyright	Copyright (C) 2006 - 2016 Fabio Esteban Uzeltinger.
+ * @email		fabiouz@gmail.com
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ // no direct access
 defined('_JEXEC') or die;
 
 jimport('joomla.application.component.controllerform');
